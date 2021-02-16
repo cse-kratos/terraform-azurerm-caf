9a189e01bd6d39b8ed1e7944c87a5ee886e34fd4
Malcolm Miller <malcolmjmil@gmail.com>
2021-02-16 23:07:42 +0000

    added acr and client config to dynamic appsettings, added acr login server url to output, added missing os_type to function app

--- locals.tf
+++ locals.tf
@@ -7,6 +7,8 @@ locals {
       keyvaults                   = local.combined_objects_keyvaults
       machine_learning_workspaces = local.combined_objects_machine_learning
       managed_identities          = local.combined_objects_managed_identities
+      azure_container_registries   = local.combined_objects_azure_container_registries
+      client_config               = tomap({ (local.client_config.landingzone_key) = {config = local.client_config} })
   }
 
   dynamic_app_config_combined_objects = {
