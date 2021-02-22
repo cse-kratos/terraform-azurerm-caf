7fe5f78c982cddcf720da0a0f575c5225bb398f7
erikschlegel <erisch@microsoft.com>
2021-02-22 01:19:12 +0000

    adding azure devops project creation capabilities to CAF

--- modules/devops/providers/azure-devops/main.tf
+++ modules/devops/providers/azure-devops/main.tf
@@ -1,13 +1,8 @@
 terraform {
   required_providers {
-    azurecaf = {
-      source = "aztfmod/azurecaf"
-    }
-
     azuredevops = {
       source = "microsoft/azuredevops"
       version = ">=0.1.0"
     }
   }
-  required_version = ">= 0.13"
 }
